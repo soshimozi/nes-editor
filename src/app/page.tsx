f11138e1291599be846712391f1104fdaef3eca5
Soshimozi <scott.mccain@gmail.com>
2025-03-30 13:47:42 -0700

    Refactored the paths and added buttons to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold">NES CHR Editor</h1>
-      <p>Click below to start editing tiles!</p>
-      <a href="/editor" className="text-blue-600 underline">Go to Editor</a>
-
-      <div className="animate-fade-in">
-        This will fade in!
+      <h1 className="text-2xl font-bold">NES Editor Tool</h1>
+      <div className="flex flex-row gap-2">
+      <a href="/tile-map-editor" className="bg-green-400 border border-zinc-800 rounded p-2">Tile Map Editor</a> 
+      <a href="/sprite-editor" className="bg-blue-400 border border-zinc-800 rounded p-2">Sprite Editor</a>
       </div>
+
     </main>
   );
 }
