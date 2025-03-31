202c59a4d9afb728d324b3e4b0cfe6279215ce87
Soshimozi <scott.mccain@gmail.com>
2025-03-30 18:08:19 -0700

    Refactored undo/redo so it actually works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="p-8">
       <h1 className="text-2xl font-bold">NES Editor Tool</h1>
       <div className="flex flex-row gap-2">
-      <a href="/tile-map-editor" className="bg-green-400 border border-zinc-800 rounded p-2">Tile Map Editor</a> 
-      <a href="/sprite-editor" className="bg-blue-400 border border-zinc-800 rounded p-2">Sprite Editor</a>
+        <a href="/tile-map-editor" className="bg-green-400 border border-zinc-800 rounded p-2">Tile Map Editor</a> 
+        <a href="/sprite-editor" className="bg-blue-400 border border-zinc-800 rounded p-2">Sprite Editor</a>
       </div>
 
     </main>
